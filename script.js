d07327fdbb9b87499c967ab411716b434be631a1
NaderMohamed325 <nadoorclasher@gmail.com>
2024-10-29 18:31:37 +0300

    V.2

--- script.js
+++ script.js
@@ -10,58 +10,80 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map, mapEvent;
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position);
+class App {
+    #map;
+    #mapEvent;
+
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleElevationField);
+    }
+
+    _getPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+                alert("Can't get your position");
+            });
+        }
+    }
+
+    _loadMap(position) {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-        let coords = [latitude - 0.053, longitude + 0.195];
+        const coords = [latitude - 0.053, longitude + 0.195];
 
-        map = L.map('map').setView(coords, 13);
-        console.log(map);
+        this.#map = L.map('map').setView(coords, 13);
+
         L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
             minZoom: 0,
             maxZoom: 20,
             attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             ext: 'jpg'
-        }).addTo(map);
+        }).addTo(this.#map);
 
-        L.marker(coords).addTo(map)
+        L.marker(coords).addTo(this.#map)
             .bindPopup('Your location')
             .openPopup();
 
         // Handling clicks on the map
-        map.on('click', function (mapE) {
-            mapEvent = mapE;
-            form.classList.remove('hidden');
-            inputDistance.focus();
-        });
-    }, function () {
-        alert("Can't get your position");
-    });
+        this.#map.on('click', this._showForm.bind(this));
+    }
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e) {
+        e.preventDefault();
+
+        // Clear input fields
+        inputDistance.value = '';
+        inputDuration.value = '';
+        inputCadence.value = '';
+        inputElevation.value = '';
+
+        // Add new marker
+        L.marker(this.#mapEvent.latlng).addTo(this.#map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            })).setPopupContent("Workout 🏃‍♂️")
+            .openPopup();
+
+        form.classList.add('hidden');
+    }
 }
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    //clear input fields
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
-    console.log(mapEvent);
-    L.marker(mapEvent.latlng).addTo(map)
-        .bindPopup(L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-        })).setPopupContent("Workout 🏃‍♂️")
-        .openPopup();
-    form.classList.add('hidden');
-});
-
-inputType.addEventListener('change', function () {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
